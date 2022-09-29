d463832a0374f7e7816065430860568488e2fd1f
Dario Pisapia <worldariolibero.it>
2022-09-29 11:23:46 +0200

    refactor rightBlock

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,32 @@
+import styles from "./styles.css"
+
+import backArrow from "../../img/BackArrow.png"
+import whiteArrow from "../../img/whiteArrow.png"
+
+export default function Footer ({ page, skip, back, onSubmit}){
+                            
+    
+    return(
+        <>
+        <div className="footer">
+            <a onClick={back}><img src={backArrow}/> {page === 1 ? ' Back to the homepage' : ' Back to the previous step'}</a>
+            <div className="buttons">
+                <button 
+                    type="button" 
+                    className="skipButton" 
+                    onClick={skip}
+                >
+                    Skip for now
+                </button>
+                <button 
+                    type="button" 
+                    className="nextButton" 
+                    onClick={onSubmit}
+                >
+                    {page < 3 ? (<> Next step <img src={whiteArrow}/></>) : 'Finish'}
+                </button>
+            </div>
+        </div>
+        </>
+    )
+}
--- src/Components/FormComponent/index.js
+++ src/Components/FormComponent/index.js
@@ -4,8 +4,51 @@ import styles from "./styles.css"
 import FirstPageForm from "../FirstPage/index"
 import SecondPageForm from "../SecondPage/index"
 import ThirdPageForm from "../ThirdPage/index"
+import Footer from "../Footer"
+import validateFirstPage from "../FirstPage/validationFunctions.js"
+import validateSecondPage from "../SecondPage/validationFunctions.js"
+import validateThirdPage from "../ThirdPage/validatationFunctions"
 
-export default function FormComponent({page, data, setData}){
+export default function FormComponent({ page, setPage }){
+
+    const [data, setData] = useState({name : "", prefix: "+39", phone: "", email: "", country:"Italy", minMoney:"10000",
+                                        maxMoney: "50000", accredited:"", interestedIn:[]})    
+                                        
+                                        
+    function handleNextClick(){
+        if (page===1){
+            if(validateFirstPage(data)){
+                setPage(page+1) 
+            }
+        }
+        if (page===2){
+            if(validateSecondPage(data))
+            setPage(page+1)
+        }
+        if (page===3){
+            if (validateThirdPage(data)){
+                    alert(
+`Name: ${data.name}
+Phone: ${data.prefix} ${data.phone}
+Email: ${data.email}
+Country: ${data.country}
+Investment between ${data.minMoney}$ and ${data.maxMoney}$
+Accredited: ${data.accredited}
+Interests: ${data.interestedIn}  `)
+
+            }
+            
+        }
+    }   
+
+    const handleSkip = () => {
+        page < 3 ? setPage(page+1) : alert('torna indietro non fare il furbo')
+    }
+
+    const handleBack = () => {
+        page > 1 ? setPage(page - 1) : alert('occhio ad andare troppo indietro')
+    }
+    
     if(page===1){
         return(
             <>
@@ -14,6 +57,7 @@ export default function FormComponent({page, data, setData}){
                 <div className="description">Welcome to United Properties, we are glad to see you! Let’s get started by letting us know a little bit about you</div>
             </div> 
             <FirstPageForm data={data} setData={setData} />
+            <Footer page={page} skip={handleSkip} back={handleBack} onSubmit={handleNextClick} />
             </>
         )
     }
@@ -25,7 +69,8 @@ export default function FormComponent({page, data, setData}){
                 <div className="title">Investment plans</div>
                 <div className="description">Let us know about your investment plans. This will help us get you to the right expert who will help you further</div>
             </div> 
-            <SecondPageForm  data={data} setData={setData}/>      
+            <SecondPageForm  data={data} setData={setData}/>
+            <Footer page={page} skip={handleSkip} back={handleBack} onSubmit={handleNextClick}  />         
             </>
         )
     }
@@ -38,6 +83,7 @@ export default function FormComponent({page, data, setData}){
                 <div className="description">This will help us figure out what your investment option are so that we can show you only possibly intresting for you deals</div>
             </div> 
             <ThirdPageForm data={data} setData={setData}/>
+            <Footer page={page} skip={handleSkip} back={handleBack} onSubmit={handleNextClick} />
             </>
         )
     }
